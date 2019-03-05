0dd5c938542c6a53e47c7dab52ae5d52c23c1f5e
Michał Borkowski <s15288@lab110l-17>
2019-03-05 10:02:57 +0100

    update AppTest.java

--- cars/src/test/java/pl/edu/pjatk/tau/AppTest.java
+++ cars/src/test/java/pl/edu/pjatk/tau/AppTest.java
@@ -1,5 +1,7 @@
 package pl.edu.pjatk.tau;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -7,6 +9,9 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class AppTest {
     
-    
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
 
 }
